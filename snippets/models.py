11dc29c05ccddc246aee1aec3f8035d363fc95f7
Vikas <vikaschandak05@gmail.com>
2021-05-27 05:35:16 +0530

    added User model

--- snippets/models.py
+++ snippets/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from pygments import styles
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 
 LEXERS = [i for i in get_all_lexers()if i[1]]
@@ -16,6 +18,18 @@ class Snippet(models.Model):
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGES, default='python', max_length=100)
     style = models.CharField(choices=STYLES,default='friendly',max_length=100)
+    owner = models.ForeignKey('auth.user', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ['created']
+    
+
+    def save(self, *args, **kwargs):
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title':self.title} if self.title else {}
+        formatter = HtmlFormatter(style = self.style, linenos = linenos, full=True, **options)
+        self.highlighted = highlight(self.code,lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
+    
